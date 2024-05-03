f22946855ccce0655b4a701e5ba5948f69003061
sudhanshu8352 <82777068+sudhanshu8352@users.noreply.github.com>
2024-05-03 23:23:48 +0530

    Basic project done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,48 @@
-import React from "react";
+import './App.css'
+import VideoPlayer from './VedioPlayer'
+import { useRef } from 'react'
+import videojs from "video.js";
+
 
 function App() {
+  const playerRef = useRef(null)
+  const videoLink = "http://localhost:8000/uploads/games/06b10511-13c5-41e0-94ff-1cf06397d91b/index.m3u8"
+
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL"
+      }
+    ]
+  }
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+
   return (
-    <div className="App">
-      App
-    </div>
-  );
+    <>
+      <div>
+        <h1>Video player</h1>
+      </div>
+      <VideoPlayer
+      options={videoPlayerOptions}
+      onReady={handlePlayerReady}
+      />
+    </>
+  )
 }
 
-export default App;
+export default App
